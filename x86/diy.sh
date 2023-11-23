d05804947c0bff020f0245824f9cd36b26018b94
森森 <kwsensen@163.com>
2023-11-23 14:45:24 +0800

    Update diy.sh

--- x86/diy.sh
+++ x86/diy.sh
@@ -2,6 +2,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.112.19/g' package/base-files/files/bin/config_generate
 
 # default feeds
 sed -i $'s/^sed -i \'s\/downloads\.openwrt\.org\/.*\/g\' \/etc\/opkg\/distfeeds\.conf$/sed -i \'s\/downloads\.openwrt\.org\/mirrors\.tuna\.tsinghua\.edu\.cn\\\\\/openwrt\/g\' \/etc\/opkg\/distfeeds\.conf/g' package/lean/default-settings/files/zzz-default-settings
